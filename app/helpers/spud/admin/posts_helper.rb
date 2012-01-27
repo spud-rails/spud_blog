f755cc66e4b34c7c9c43a85c907963c87c89ce15
Greg Woods <greg@westlakedesign.com>
2012-01-27 17:01:00 -0500

    category add form (unfinished)

--- app/helpers/spud/admin/posts_helper.rb
+++ app/helpers/spud/admin/posts_helper.rb
@@ -1,2 +1,15 @@
 module Spud::Admin::PostsHelper
+
+	def options_for_parent_category(parent_id = 0)
+		if @categories[parent_id]
+			return @categories[parent_id].collect{ |c| 
+				opts = [c.name, c.id] 
+				opts += options_for_parent_category(c.id)
+			}
+		else
+			return []
+		end
+
+	end
+
 end
