8bddfae002dfc6ba5f795ae70b35b4d282b65a01
Greg Woods <greg@westlakedesign.com>
2012-02-13 10:30:05 -0500

    added config options to turn on/off the news and blog apps. by default news is off and blog is on.

--- lib/spud_blog/configuration.rb
+++ lib/spud_blog/configuration.rb
@@ -1,8 +1,9 @@
 module Spud
   module Blog
     include ActiveSupport::Configurable
-    config_accessor :base_layout, :news_enabled
+    config_accessor :base_layout, :blog_enabled, :news_enabled
     self.base_layout = 'application'
     self.news_enabled = false
+    self.blog_enabled = true
   end
 end
--- lib/spud_blog/engine.rb
+++ lib/spud_blog/engine.rb
@@ -5,21 +5,27 @@ module Spud
       engine_name :spud_blog
       initializer :admin do
         Spud::Core.config.admin_applications += [{
-          :name => 'Blog Posts', 
-          :thumbnail => 'spud/admin/posts_thumb.png',
-          :url => '/spud/admin/posts',
-          :order => 1
-        },{
-          :name => 'News Posts',
-          :thumbnail => 'spud/admin/posts_thumb.png',
-          :url => '/spud/admin/news_posts',
-          :order => 2
-        },{
           :name => 'Post Categories',
           :thumbnail => 'spud/admin/posts_thumb.png',
           :url => '/spud/admin/post_categories',
           :order => 3
         }]
+        if Spud::Blog.config.blog_enabled
+          Spud::Core.config.admin_applications += [{
+            :name => 'Blog Posts', 
+            :thumbnail => 'spud/admin/posts_thumb.png',
+            :url => '/spud/admin/posts',
+            :order => 1
+          }]
+        end
+        if Spud::Blog.config.news_enabled
+          Spud::Core.config.admin_applications += [{
+            :name => 'News Posts',
+            :thumbnail => 'spud/admin/posts_thumb.png',
+            :url => '/spud/admin/news_posts',
+            :order => 2
+          }]
+        end
       end
       initializer :assets do
         Rails.application.config.assets.precompile += ['spud/admin/posts.css']
