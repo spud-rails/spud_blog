872716d68148180ca0aeef9dabaf5c38a775e987
Greg Woods <greg@westlakedesign.com>
2012-02-13 11:43:27 -0500

    commented routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,28 +11,34 @@ Rails.application.routes.draw do
     end
   end
 
+  # Blog Post Categories
   get 'blog/category/:category_url_name', :controller => 'blog', :action => 'category', :page => 1, :as => 'blog_category'
   get 'blog/category/:category_url_name/page/:page', :controller => 'blog', :action => 'category'
   post 'blog/category', :controller => 'blog', :action => 'category'
 
+  # Blog Post Archives
   get 'blog/archive/:archive_date', :controller => 'blog', :action => 'archive', :page => 1, :as => 'blog_archive'
   get 'blog/archive/:archive_date/page/:page', :controller => 'blog', :action => 'archive'
   post 'blog/archive', :controller => 'blog', :action => 'archive'
 
+  # Blog Posts
   get 'blog', :controller => 'blog', :action => 'index', :page => 1
   get '/blog/page/:page', :controller => 'blog', :action => 'index'
   resources :blog_posts, :path => 'blog', :controller => 'blog', :only => [:show] do
     post '/', :on => :member, :controller => 'blog', :action => 'create_comment'
   end
 
+  # News Post Categories
   get 'news/category/:category_url_name', :controller => 'news', :action => 'category', :page => 1, :as => 'news_category'
   get 'news/category/:category_url_name/page/:page', :controller => 'news', :action => 'category'
   post 'news/category', :controller => 'news', :action => 'category'
   
+  # News Post Archives
   get 'news/archive/:archive_date', :controller => 'news', :action => 'archive', :page => 1, :as => 'news_archive'
   get 'news/archive/:archive_date/page/:page', :controller => 'news', :action => 'archive'
   post 'news/archive', :controller => 'news', :action => 'archive'
   
+  # News Posts
   get 'news', :controller => 'news', :action => 'index', :page => 1
   get '/news/page/:page', :controller => 'news', :action => 'index'
   resources :news_posts, :path => 'news', :controller => 'news', :only => [:show]
