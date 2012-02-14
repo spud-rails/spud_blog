36e1b0e624619c6cd6fdb9d29931d54bf87edb26
Greg Woods <greg@westlakedesign.com>
2012-02-14 10:49:06 -0500

    woops

--- db/migrate/20120127144942_add_url_to_spud_post_categories.rb
+++ db/migrate/20120127144942_add_url_to_spud_post_categories.rb
@@ -1,6 +1,6 @@
 class AddUrlToSpudPostCategories < ActiveRecord::Migration
   def change
-  	add_column :spud_post_categories, :parent_id, :integer, :default => false
+  	add_column :spud_post_categories, :parent_id, :integer, :default => 0
   	add_column :spud_post_categories, :url_name, :string
   	add_index :spud_post_categories, :parent_id
   	add_index :spud_post_categories, :url_name
