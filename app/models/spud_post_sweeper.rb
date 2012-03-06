e2b4a884dc97781a36d89c360c556e771c63f2d5
Greg Woods <greg@westlakedesign.com>
2012-03-06 09:49:11 -0500

    fix for sitemap sweeper

--- app/models/spud_post_sweeper.rb
+++ app/models/spud_post_sweeper.rb
@@ -38,7 +38,7 @@ class SpudPostSweeper < ActionController::Caching::Sweeper
       end
     end
 
-    expire_page spud_sitemap_path
+    expire_page spud_sitemap_path(:format => :xml)
   end
 
 end
