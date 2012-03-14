9fc159b8db9dbd1043e7cd4d6632148a0566619d
Greg Woods <greg@westlakedesign.com>
2012-03-14 10:08:30 -0400

    fixed some more sweeper issues.

--- app/models/spud_post_category_sweeper.rb
+++ app/models/spud_post_category_sweeper.rb
@@ -15,26 +15,26 @@ class SpudPostCategorySweeper < ActionController::Caching::Sweeper
   def expire_cache_for(record)
     if Spud::Blog.config.enable_action_caching
       SpudPost.find_each do |p|
-        if p.is_news
+        if p.is_news && Spud::Blog.config.news_enabled
           expire_action news_post_url(p.url_name)
-        else
+        elsif Spud::Blog.config.blog_enabled
           expire_action blog_post_url(p.url_name)
         end
       end
-      expire_action news_url
-      expire_action blog_url
+      expire_action news_url if Spud::Blog.config.news_enabled
+      expire_action blog_url if Spud::Blog.config.blog_enabled
       expire_action spud_blog_sitemap_url
     end
     if Spud::Blog.config.enable_full_page_caching
       SpudPost.find_each do |p|
-        if p.is_news
+        if p.is_news && Spud::Blog.config.news_enabled
           expire_page news_post_path(p.url_name)
-        else
+        elsif Spud::Blog.config.blog_enabled
           expire_page blog_post_path(p.url_name)
         end
       end
-      expire_page news_path
-      expire_page blog_path
+      expire_page news_path if Spud::Blog.config.news_enabled
+      expire_page blog_path if Spud::Blog.config.blog_enabled
       expire_page spud_blog_sitemap_path(:format => :xml)
     end
   end
