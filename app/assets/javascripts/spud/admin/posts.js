12916e966f1fe362535eb7abebb322c2912680d2
Greg Woods <greg@westlakedesign.com>
2012-11-12 11:36:39 -0500

    Allow creation of categories from directly within posts form. Styling needed

--- app/assets/javascripts/spud/admin/posts.js
+++ app/assets/javascripts/spud/admin/posts.js
@@ -13,6 +13,9 @@ Spud.Admin.Posts = new function(){
 		initDatePicker();
 		initTinyMCE();
 		$('#spud_post_new_category_form button').live('click', self.didSubmitCategory);
+		$('.spud_post_add_category').live('click', self.clickedPostAddCategory);
+		$('.save_post_category_button').live('click', self.clickedSavePostCategory);
+		$('.spud_post_category_form').live('submit', self.submittedPostCategoryForm);
 	};
 
 	this.didSubmitCategory = function(event){
@@ -25,5 +28,67 @@ Spud.Admin.Posts = new function(){
 				'spud_post_category[parent_id]': $('#spud_post_new_category_parent_id').val()
 			}
 		});
+	};
+
+	this.clickedPostAddCategory = function(e){
+		e.preventDefault();
+		$.ajax({
+			url: $(this).attr('href'),
+			dataType: 'html',
+			success: function(html, textStatus, jqXHR){
+				displayModalDialogWithOptions({
+					title: 'Add Category',
+					html: html,
+					buttons: {
+						save_post_category_button: 'Save'
+					}
+				});
+			}
+		});
+	};
+
+	this.clickedSavePostCategory = function(e){
+		e.preventDefault();
+		self.submittedPostCategoryForm();
+	};
+
+	this.submittedPostCategoryForm = function(e){
+		var form = $('.spud_post_category_form');
+		$.ajax({
+			url: form.attr('action'),
+			data: form.serialize(),
+			type: 'post',
+			dataType: 'json',
+			success: self.savedPostCategorySuccess,
+			error: self.savePostCategoryError
+		});
+		return false;
+	};
+
+	this.savedPostCategorySuccess = function(data, textStatus, jqXHR){
+		var checkbox = '';
+		checkbox += '<li class="spud_post_form_category">';
+		checkbox += '<input id="spud_post_category_'+data.id+'" name="spud_post[category_ids][]" type="checkbox" value="'+data.id+'">';
+		checkbox += '<label for="spud_post_category_'+data.id+'">'+data.name+'</label>';
+		checkbox += '</li>';
+		if(data.parent_id > 0){
+			$('.spud_post_form_category[data-id="'+data.parent_id+'"] ul').prepend(checkbox);
+		}
+		else{
+			$('.spud_post_categories_form').prepend(checkbox);
+		}
+		hideModalDialog();
+	};
+
+	this.savePostCategoryError = function(jqXHR, textStatus, errorThrown){
+		if(jqXHR.status == 422){
+			var html = jqXHR.responseText;
+			$('.spud_post_category_form').replaceWith(html);
+		}
+		else{
+			if(window.console){
+        console.error('Oh Snap:', arguments);
+      }
+		}
 	};
 };
--- app/controllers/spud/admin/post_categories_controller.rb
+++ app/controllers/spud/admin/post_categories_controller.rb
@@ -1,7 +1,7 @@
 class Spud::Admin::PostCategoriesController < Spud::Admin::ApplicationController
 
-	layout 'spud/admin/post'
-	respond_to :html, :xml, :json
+	layout :get_layout
+	respond_to :html, :xml, :json, :js
 	before_filter :find_category, :only => [:show, :edit, :update, :destroy]
 	add_breadcrumb 'Post Categories', :spud_admin_post_categories_path
 	belongs_to_spud_app :post_categories
@@ -34,8 +34,10 @@ class Spud::Admin::PostCategoriesController < Spud::Admin::ApplicationController
 		if @post_category.save
 			flash[:notice] = 'Post Category was successfully created'
 			expire_post_actions
+			respond_with @post_category, :location => spud_admin_post_categories_path
+		else
+			render 'new', :status => 422
 		end
-		respond_with @post_category, :location => spud_admin_post_categories_path
 	end
 
 	def destroy
@@ -55,6 +57,14 @@ class Spud::Admin::PostCategoriesController < Spud::Admin::ApplicationController
 	def expire_post_actions
 		expire_action news_url if Spud::Blog.config.news_enabled
 		expire_action blog_url if Spud::Blog.config.blog_enabled
+	end
+
+	def get_layout
+		if request.xhr?
+			return nil
+		else
+			return 'spud/admin/post'
+		end
 	end
 
 end
