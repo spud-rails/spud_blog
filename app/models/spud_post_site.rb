b5d7e481d2e5d6bd390d2addc901240089244203
David Estes <davydotcom@gmail.com>
2014-09-03 15:09:04 -0400

    Fixed bug in multisite

--- app/models/spud_post_site.rb
+++ app/models/spud_post_site.rb
@@ -1,6 +1,6 @@
 class SpudPostSite < ActiveRecord::Base
   # attr_accessible :spud_post_id, :spud_site_id
-  belongs_to :spud_post, :touch => :true
+  belongs_to :spud_post, :touch => true
 
   def spud_site
     return Spud::Core.site_config_for_id(spud_site_id)
